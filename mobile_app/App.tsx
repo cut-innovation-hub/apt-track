efe6f2517d7652d4f936cbfe02e1ceb9b2c8a5ae
tatendabakozw <tatendabakozw@gmail.com>
2022-06-16 23:37:39 +0200

    fix: added autocomplete module

--- mobile_app/App.tsx
+++ mobile_app/App.tsx
@@ -1,12 +1,23 @@
-import { StyleSheet } from 'react-native';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import HomeScreen from './screens/HomeScreen/HomeScreen';
+import { StyleSheet } from "react-native";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import HomeScreen from "./screens/HomeScreen/HomeScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <Provider store={store}>
-      <HomeScreen />
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={{ headerShown: false }}>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
     </Provider>
   );
 }
@@ -14,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
