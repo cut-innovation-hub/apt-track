92568274f83296237d1e533646c925622cdbeaf3
tatendabakozw <tatendabakozw@gmail.com>
2022-07-25 14:25:16 +0200

    feat: added dashboard layout

--- admin_dasboard/pages/_app.tsx
+++ admin_dasboard/pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { StoreProvider } from "../context/Store";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <StoreProvider>
+        <Component {...pageProps} />
+      </StoreProvider>
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- admin_dasboard/tailwind.config.js
+++ admin_dasboard/tailwind.config.js
@@ -1,11 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        blue: {
+          primary: "#0e7490",
+          secondary: "#5469D0",
+          dark: "#164e63",
+          light: "#06b6d4",
+          superlight: "#67e8f9",
+        },
+        new: {
+          primary: "#ad596d",
+          light: "#bf7e8e",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
